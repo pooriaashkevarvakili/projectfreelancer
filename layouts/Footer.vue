4916fb6a1dfa465fa2ab6aead44fde46ebe25400
pooria Vakili <pooriavakili09@gmail.com>
2023-12-05 05:48:16 +0330

    update-code

--- layouts/Footer.vue
+++ layouts/Footer.vue
@@ -16,7 +16,7 @@
                 <div class="footer-about footer-column-border col-xl-6 col-lg-6">
                     <div class="inner">
                         <div class="footer-logo text-white iran-sans">وبگاه رسمی فنام</div>
-                        <div class="text iran-sans">
+                        <div class="text about-footer-matn-one iran-sans">
                             شرکت فناوری اطلاعات کارآفرین خاص، با نام سابق فناوری مالی کارآفرینی امید (فنام) در سال 1376، به عنوان شرکت سهامی خاص و با شناسه ملی 10101738541 ثبت شده است. این شرکت در حوزه It با خدماتی از قبیل مشاوره، طراحی، توسعه و پشتیبانی سامانه‌های نرم افزاری و موبایلی و نیز فروش و پشتیبانی پایانه‌های فروشگاهی در حال فعالیت است.
 
                         </div>
